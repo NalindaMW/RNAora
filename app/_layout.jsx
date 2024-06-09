99581efdd8d5700683257fc8cd0f52b43087f139
Nalinda Wickramarathna <mastersilicon@gmail.com>
2024-06-09 13:15:06 +0530

    Custom fonts installed

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen, Stack } from 'expo-router'
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   // Using Slot -> Taking index.jsx as a default page
@@ -13,6 +16,26 @@ const RootLayout = () => {
     </>
   )
   */
+
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if(error) throw error;
+
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error])
+  
+  if(!fontsLoaded && error) return null;
  
   // Using Stack
   return (
